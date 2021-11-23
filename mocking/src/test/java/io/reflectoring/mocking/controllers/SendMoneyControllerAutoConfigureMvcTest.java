458952b496bc3d5f690d69731fce124c31d724d3
PierreSQS <nomo_messina@web.de>
2021-11-23 12:09:10 +0100

    Implemented AutoConfigureMvc Test

--- mocking/src/test/java/io/reflectoring/mocking/controllers/SendMoneyControllerAutoConfigureMvcTest.java
+++ mocking/src/test/java/io/reflectoring/mocking/controllers/SendMoneyControllerAutoConfigureMvcTest.java
@@ -4,32 +4,30 @@ import io.reflectoring.mocking.services.SendMoneyService;
 import io.reflectoring.mocking.services.SendMoneyUseCase;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 
-@ExtendWith(MockitoExtension.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 class SendMoneyControllerAutoConfigureMvcTest {
 
-    @Mock
+    @MockBean
     SendMoneyService sendMoneyServMock;
 
-    @InjectMocks
-    SendMoneyController sendMoneyController;
-
+    @Autowired
     MockMvc mockMvc;
 
     @BeforeEach
     void setUp() {
-        mockMvc = standaloneSetup(sendMoneyController).build();
+
     }
 
     @Test
